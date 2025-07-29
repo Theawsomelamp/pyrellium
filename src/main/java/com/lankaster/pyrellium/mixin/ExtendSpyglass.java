e1baee26ae93d8babeb613e503d228ddf28e879d
Theawesomelamp <sebasrobers@gmail.com>
2025-07-29 16:11:50 +0200

    Offhand spyglass fix

--- src/main/java/com/lankaster/pyrellium/mixin/ExtendSpyglass.java
+++ src/main/java/com/lankaster/pyrellium/mixin/ExtendSpyglass.java
@@ -16,7 +16,7 @@ public abstract class ExtendSpyglass {
 
     @Inject(method = "isUsingSpyglass", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"), cancellable = true)
     public void isUsingSpyglass(CallbackInfoReturnable<Boolean> cir) {
-        if (getEquippedStack(EquipmentSlot.MAINHAND).isOf(ModItems.OPAL_SPYGLASS)) {
+        if (getEquippedStack(EquipmentSlot.MAINHAND).isOf(ModItems.OPAL_SPYGLASS) || getEquippedStack(EquipmentSlot.OFFHAND).isOf(ModItems.OPAL_SPYGLASS)) {
             cir.setReturnValue(true);
         }
     }
