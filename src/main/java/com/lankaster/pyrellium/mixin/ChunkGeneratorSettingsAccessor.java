44022f466aa7152e62964f35c0c06d74cbeeb167
Theawesomelamp <sebasrobers@gmail.com>
2025-10-10 11:42:44 +0200

    Separated surface rules from noise settings

---
+++ src/main/java/com/lankaster/pyrellium/mixin/ChunkGeneratorSettingsAccessor.java
@@ -0,0 +1,13 @@
+package com.lankaster.pyrellium.mixin;
+
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
+import net.minecraft.world.gen.surfacebuilder.MaterialRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ChunkGeneratorSettings.class)
+public interface ChunkGeneratorSettingsAccessor {
+    @Accessor("surfaceRule") @Mutable
+    void addSurfaceRule(MaterialRules.MaterialRule ruleSource);
+}
